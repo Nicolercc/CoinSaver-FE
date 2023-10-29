9f429b364fe0203054c2e0db17b8413e0c9bfdc5
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-10-29 16:20:57 -0400

    added jsx and css

--- src/components/NewItemForm/NewItemForm.js
+++ src/components/NewItemForm/NewItemForm.js
@@ -43,7 +43,81 @@ function NewItemForm({ setTransactionList, transactionList }) {
     }
   }
 
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="m-5 title">Add new item</h1>
+      <form className="m-5" onSubmit={handleSubmit}>
+        <div className="m-5">
+          <label htmlFor="date">Date</label>
+          <input
+            type="date"
+            value={formData.date}
+            id="date"
+            name="date"
+            className="form-control form-control-lg"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="m-5">
+          <label htmlFor="item_name">Item Name</label>
+          <input
+            type="text"
+            value={formData.item_name}
+            id="item_name"
+            name="item_name"
+            className="form-control form-control-lg"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="m-5">
+          <label htmlFor="amount">Amount</label>
+          <input
+            type="number"
+            value={formData.amount}
+            id="amount"
+            name="amount"
+            className="form-control form-control-lg"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="m-5">
+          <label htmlFor="from">From</label>
+          <input
+            type="text"
+            value={formData.from}
+            id="from"
+            name="from"
+            className="form-control form-control-lg"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="m-5">
+          <label htmlFor="category">Category</label>
+          <select
+            id="category"
+            name="category"
+            onChange={handleChange}
+            className="my-3 form-select"
+          >
+            <option></option>
+            <option>Salary</option>
+            <option>Entertainment</option>
+            <option>Savings</option>
+            <option>Expenses</option>
+            <option>Taxes</option>
+            <option>Retirement</option>
+          </select>
+        </div>
+        <div className="row">
+          <div className="col text-center">
+            <button type="submit" className="btn btn-primary">
+              CREATE NEW ITEM
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default NewItemForm;

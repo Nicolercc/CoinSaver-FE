466260891bfda0d21c162ec34fe0d68631f7bb98
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-10-29 16:11:09 -0400

    Home jsx

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,12 +22,40 @@ function Home() {
       <div className="container">
         <div className="loading-container">
           <h2>Loading...</h2>
-          {/* You can add a loading spinner or animation here */}
         </div>
       </div>
     );
   }
-  return <div>Home</div>;
+
+  return (
+    <div className="container">
+      <div className="table-container">
+        <table className="table table-hover ">
+          <tbody>
+            {transactionsArray.map(
+              ({ item_name, date, category, amount, id }) => {
+                const formattedDate = formatDate(date);
+                return (
+                  <tr key={id} className="table-row">
+                    <td className="table-cell fs-4">{formattedDate}</td>
+                    <td className="table-cell fs-4">
+                      <Link
+                        className="category-link text "
+                        to={`/transaction/${id}`}
+                      >
+                        {item_name}
+                      </Link>
+                    </td>
+                    <td className="table-cell fs-4">${amount}</td>
+                  </tr>
+                );
+              }
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
